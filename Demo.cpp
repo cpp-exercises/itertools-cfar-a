b01080dc7c9087feffd09cab306290c29e93afae
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-06-05 03:45:09 +0300

    change iterables

--- Demo.cpp
+++ Demo.cpp
@@ -1,85 +1,70 @@
 //
 //  Demo for itertools exercise.
 //
-//  Created by Adam Lev-Ari on 09/02/2020.
-//  Copyright © 2020 Adam Lev-Ari. All rights reserved.
+//  Version 1 by Adam Lev-Ari on 02/2020. Copyright © 2020 Adam Lev-Ari. All rights reserved.
+//  Version 2 by Erel Segal-Halevi on 06/2020.
 //
 
 #include <iostream>
 #include <vector>
 
+#include "range.hpp"
 #include "accumulate.hpp"
-#include "cycle.hpp"
-#include "filter_false.hpp"
-#include "permutation.hpp"
-#include "zip_longest.hpp"
-#include "output.hpp"
+#include "filterfalse.hpp"
+#include "compress.hpp"
+
 
 using namespace itertools;
 using namespace std;
 
 
-
-struct lessThan
-{
-    template<typename T>
-    bool operator()(T& i) const { return i < 3; }
+struct lessThan3 {
+    bool operator()(int i) const { return i < 3; }
 };
 
 int main(int argc, const char * argv[]) {
     
-    vector<int> vecInit = {1,2,3,4};
+    cout << "####  range:  ####";
+	for (int i: range(5,9))
+		cout << i << " ";      // 5 6 7 8
+
+    vector<int> vecInt = {1,2,3,4};
     vector<string> vecString = {"Hello", "Bye", "Adam"};
     vector<float> vecFloat = {-1, 0.3, 5.2, -8.3};
     
-    
-    //Defined a final int for output
-    cout << "####  cycle:  ####" << endl;
-    cout << "Finite<limit: 5 times> cycle of numbers vector: " << endl;
-    for (auto i: cycle(vecInit,5))
-        cout << i;    // 1234 1234 1234 ...
-    cout << endl << endl;
-    
     cout << "####  accumulate:  ####";
+    cout << endl << "accumulate of range: " << endl;
+	for (int i: accumulate(range(5,9)))
+		cout << i << " ";      // 5 11 18 26
+
     cout << endl << "accumulate of vector<string>: " << endl;
     for (auto i: accumulate(vecString) )
-        cout << i << " ";
-    cout << endl << endl;
-    
-    cout << "####  accumulate & cycle:  ####";
-    cout << endl << "cycle of accumulate of string vector: " << endl;
-    for (auto i: cycle(accumulate(vecString),2) )
-        cout << i << " ";
-    cout << endl << endl;
-    
-    cout << "####  zip_longest:  ####";
-    cout << endl << "zip longest of vector<float> & vector<int>: " << endl;
-    for (auto i: zip_longest(vecFloat, vecInit) )
-        cout << i << " ";
-    cout << endl << endl;
-    
-    cout << "####  zip_longest & cycle:  ####";
-    cout << endl << "zip longest of cycle<vector:{1,2,3,4}> & cycle<vector:{-1, 0.3, 5.2, -8.3}>: " << endl;
-    for (auto i: zip_longest(cycle(vecInit,2), cycle(vecFloat,2)) )
-        cout << i << " ";
+        cout << i << " ";  // Hello HelloBye HelloByeAdam
     cout << endl << endl;
 
-    cout << "####  permutation:  ####";
-    cout << endl << "permutation of vector:{1,2,3,4}" << endl;
-    for (auto i: permutation(vecInit) )
-        cout << i;
-    cout << endl << endl;
+    cout << endl << "accumulate of range with binary operator: " << endl;
+	for (int i: accumulate(range(5,9), [](int x, int y){return x*y;}))
+		cout << i << " ";      // 5 30 210 1680
 
     cout << "####  Filter False:  ####";
-    cout << endl << "Filter all numbers less than 3 in vector{1,2,3,4}: " << endl;
-    for (auto i: filter_false(vecInit, lessThan()) )
-    cout << i << " ";
+    cout << endl << "Filter out all numbers less than 3 in vector{1,2,3,4}: " << endl;
+    for (auto i: filterfalse(lessThan3{}, vecInt) )
+        cout << i << " ";   // 3 4
+    cout << endl << "Filter out all even numbers in range(5,9): " << endl;
+    for (auto i: filterfalse([](int i){return i%2==0;}, range(5,9)) )
+        cout << i << " ";   // 5 7
     cout << endl << endl;
 
-    
+    cout << "####  compress:  ####";
+    cout << endl << "compress a string" << endl;
+    for (auto i: compress(string("abcd"), vector<bool>({true,true,false,true})) )
+        cout << i << " ";  // a b d
+    cout << endl << "compress a range" << endl;
+    for (auto i: compress(range(5,9), vector<bool>({true,true,false,true})) )
+        cout << i << " ";  // 5 6 8
+    cout << endl << endl;
+
+
     cout << endl;
     return 0;
 }
-
-
-
